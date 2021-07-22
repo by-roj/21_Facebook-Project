ce8901c770a36b341c0a59959a8b3c789033d8a6
윤상우 <tyuiml84@gmail.com>
2021-07-22 13:20:49 +0900

    Update by SangWoo

--- Dictionary_Line.py
+++ Dictionary_Line.py
@@ -9,9 +9,9 @@ matplotlib.rc('font',family=fornName)
 
 news_data = {'코로나':60, '올림픽':80, '폭염':67, '비트코인':43, '주식':50, '방역':76, '거리두기':70, 'MZ세대':30, '전세':21, '집값':47 }
 
-myList = news_data.items()
-myList = sorted(myList)
-x, y = zip(*myList)
+Data = news_data.items()
+myList = sorted(Data)
+x, y = zip(*Data)
 
 plt.plot(x, y, 'r') 
 plt.xlabel('해시태그')
@@ -19,3 +19,4 @@ plt.ylabel('기사 수')
 plt.title('연합뉴스 해시태그별 기사 수')
 plt.axis([0,10, 0, 110]) # x: 0 ~ 150, y:0 ~ 10
 plt.show()
+plt.subplots_adjust(hspace=11)
