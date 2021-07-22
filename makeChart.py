e622859e734b9f6151b70c519f86f1c14f136f0c
Lee-YongHa <lvur_@naver.com>
2021-07-22 16:14:00 +0900

    Update selecting time period function

---
+++ makeChart.py
@@ -0,0 +1,64 @@
+import json
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+with open("C:/Users/user/LYH/ModuleProject1/FacebookProject/data/news_data.json", "r", encoding='utf-8') as filedata:
+    newsdata = json.loads(filedata.read())
+
+# pubDate 기준으로 정렬
+sorted_arr = sorted(newsdata, key=lambda x: (x['pubDate']))
+# print(sorted_arr)
+
+now = datetime.now()    # 현재
+time_period = "year"
+num = 3
+
+if time_period == "day":
+    before_time = now - relativedelta(days=num)   # num일 전
+elif time_period == "week":
+    before_time = now - relativedelta(weeks=num)   # num주 전
+elif time_period == "month":
+    before_time = now - relativedelta(months=num)   # num달 전
+elif time_period == "year":
+    before_time = now - relativedelta(years=num)   # num년 전
+
+result = {"코로나":0, "올림픽":0, "폭염":0}
+for i in result:
+    for j in sorted_arr:
+        if datetime.strptime(j['pubDate'], '%Y-%m-%d %H:%M:%S') < before_time:  # pubDate 키의 값을 datetime 형식으로 변환
+            continue
+        else:
+            # print(j['pubDate'])
+            if (i in j['title']) or (i in j['description']) :
+                result[i] += j['title'].count(i)
+                result[i] += j['description'].count(i)
+
+    print("keyword : %s, count : %d" %(i, result[i]))
+
+news_data = list(result.items())
+
+fontLocation = r"C:\Windows\Fonts\malgun.ttf"
+fontName = font_manager.FontProperties(fname=fontLocation).get_name()
+plt.rc('font', family=fontName)
+
+fig = plt.figure(figsize=(5,5)) # 캔버스 생성
+fig.set_facecolor('white') # 캔버스 배경색을 하얀색으로 설정
+ax = fig.add_subplot() # 프레임 생성
+
+# labels = []
+# frequency = []
+# for x, y in news_data:
+#     labels.append(x)
+#     frequency.append(y)
+
+pie = ax.pie([i[1] for i in news_data],
+            startangle=90,
+            counterclock=False,
+            autopct=lambda p : '{:.2f}%'.format(p),
+            wedgeprops=dict(width=1),
+            colors=['cornflowerblue', 'yellowgreen', 'salmon'])
+
+plt.legend(pie[0], [i[0] for i in news_data])
+plt.show()
