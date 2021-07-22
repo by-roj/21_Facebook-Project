7dae73502ccc76718e55d87ab8b2be2976133404
윤상우 <tyuiml84@gmail.com>
2021-07-22 13:21:51 +0900

    Update by SangWoo

--- keywordCount_Pie.py
+++ keywordCount_Pie.py
@@ -2,7 +2,7 @@ import json
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
-with open("C:/Users/user/LYH/ModuleProject1/FacebookProject/news_data.json", "r", encoding='utf-8') as filedata:
+with open("C:\ModuleProject1\FacebookProject\corona_naver_news.json", "r", encoding='utf-8') as filedata:
     newsdata = json.loads(filedata.read())
 
 # pubDate 기준으로 정렬
