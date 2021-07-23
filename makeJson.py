f43f7b8e52dc6df62d1ded86e6942e61b59c078e
Lee-YongHa <lvur_@naver.com>
2021-07-23 13:05:12 +0900

    Move files

--- makeJson.py
+++ makeJson.py
@@ -1,15 +1,15 @@
 import json
 
-with open("C:/Lab/hot_naver_news.json", encoding='utf-8') as file_1:
+with open(("C:/Users/user/LYH/ModuleProject1/FacebookProject/data/%s_naver_news_test.json") % '코로나', encoding='utf-8') as file_1:
 	data1 = json.loads(file_1.read())
 
-with open("C:\Lab\corona_naver_news.json", encoding='utf-8') as file_2:
+with open(("C:/Users/user/LYH/ModuleProject1/FacebookProject/data/%s_naver_news_test.json") % '올림픽', encoding='utf-8') as file_2:
 	data2 = json.loads(file_2.read())
 
-with open("C:\Lab\olympic_naver_news.json", encoding='utf-8') as file_3:
+with open(("C:/Users/user/LYH/ModuleProject1/FacebookProject/data/%s_naver_news_test.json") % '폭염', encoding='utf-8') as file_3:
 	data3 = json.loads(file_3.read())
 
-with open('news_data.json', 'w', encoding='utf-8') as filedata:
+with open('C:/Users/user/LYH/ModuleProject1/FacebookProject/data/news_data_test.json', 'w', encoding='utf-8') as filedata:
     rJson = json.dumps(data1 + data2 + data3, 
                         indent=4, 
                         sort_keys=True,
